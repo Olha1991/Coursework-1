fbe478bedab5bbd8fa90a913239912f83659d594
Homework1 <danisholha760@gmail.com>
2025-03-06 00:29:11 -0600

    The basic tasks of the coursework have been completed.

---
+++ src/Employees.java
@@ -0,0 +1,63 @@
+import java.util.Objects;
+
+public class Employees {
+    private String firstName;
+    private String lastName;
+    private int department;
+    private double salary;
+    private int id;
+    private  static int counter;
+
+    public Employees(String firstName, String lastName, int department, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.department = department;
+        this.salary = salary;
+        id = counter++;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employees employee)) return false;
+        return department == employee.department && Double.compare(salary, employee.salary) == 0 && Objects.equals(firstName, employee.firstName) && Objects.equals(lastName, employee.lastName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(firstName, lastName, department, salary);
+    }
+
+    @Override
+    public String toString() {
+        return "Employee " + (id + 1) + " - " +
+                firstName + " " +
+                lastName +
+                ", department = " + department +
+                ", salary = " + salary;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,90 @@
 public class Main {
+
+    static Employees[] employee = new Employees [10];
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        employee[0] = new Employees ("Sarah", "Smith",5,6550);
+        employee[1] = new Employees ("Emily", "King",2,6250);
+        employee[2] = new Employees ("Mark", "Bennet",3,5685);
+        employee[3] = new Employees ("Lisa", "Miller",1,5650);
+        employee[4] = new Employees ("Jason", "Davis",5,6500);
+        employee[5] = new Employees ("Kevin", "Brown",4,5800);
+        employee[6] = new Employees ("Betty", "Ford",2,5600);
+        employee[7] = new Employees ("Gary", "Peterson",3,6300);
+        employee[8] = new Employees ("Nicholas", "Vargas",1,7900);
+        employee[9] = new Employees ("Donna", "Kim",4,5850);
+        printAllEmployees();
+        System.out.println("Amount of salary expenses per month - " + sumSalary() + " dollars.");
+        System.out.println("Employee with the minimum salary - " + minSalary() + " dollars.");
+        System.out.println("Employee with the maximum salary - " + maxSalary() + " dollars.");
+        System.out.println("Average salary - " + averageSalary() + " dollars.");
+        fullNameEmployees();
+    }
+
+    public static double sumSalary (){
+        double sum = 0;
+        for (int i = 0; i < employee.length; i++) {
+            if (employee[i] != null) {
+                sum += employee[i].getSalary();
+            }
+        }
+        return sum;
+    }
+
+    public static Employees minSalary (){
+        Employees minSalaryEmployee = employee[0];
+        for(int i = 0; i < employee.length; i++) {
+            if (employee[i].getSalary() < minSalaryEmployee.getSalary()) {
+                minSalaryEmployee = employee[i];
+            }
+        }
+        return minSalaryEmployee;
+    }
+
+    public static Employees maxSalary (){
+        double maxSalary = 0;
+        Employees maxSalaryEmployee = null;
+        int x = 0;
+        for (int i = 0; i < employee.length; i++) {
+            if (employee[i] != null){
+                maxSalary = maxSalary + employee[i].getSalary();
+                maxSalaryEmployee = employee[i];
+                x = i;
+                break;
+            }
+        }
+        for (int i = x; i < employee.length; i++) {
+            if (employee[i] != null){
+                if (employee[i].getSalary() > maxSalary){
+                    maxSalary = employee[i].getSalary();
+                    maxSalaryEmployee = employee[i];
+                }
+            }
+        }
+        return maxSalaryEmployee;
+    }
+
+    public static double averageSalary (){
+        if (employee.length != 0){
+            return sumSalary() / employee.length;
+        }else {
+            return 0;
+        }
+    }
+
+    public static void printAllEmployees (){
+        for (int i = 0; i < employee.length; i++){
+            if (employee [i] != null){
+                System.out.println(employee[i].toString());
+            }
+        }
+    }
+
+    public static void fullNameEmployees (){
+        for (int i = 0; i < employee.length; i++) {
+            if (employee[i] != null){
+                System.out.println(employee[i].getFirstName() + " " + employee[i].getLastName());
+            }
+        }
     }
 }
